3195f4cf065ad23c4694d2fe63909713829be0ad
hank0982 <hank0982@gmail.com>
2019-10-19 11:41:50 +0800

    fix security alert

--- src/app/option/option.component.ts
+++ src/app/option/option.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./option.component.scss']
 })
 export class OptionComponent implements OnInit {
-
-  constructor() { }
+  options=[
+    {
+      o_id: 0,
+      option: "Option 1",
+      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as the",
+    },
+    {
+      o_id: 1,
+      option: "Option 2",
+      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as the",
+    },
+    {
+      o_id: 2,
+      option: "Option 1",
+      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as the",
+    },
+  ]
+  votes=new Array(this.options.length).fill(0);
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
+  modifyVotesByID(id, value){
+    this.votes[id]=this.votes[id]+value;
+    console.log(this.votes);
+  }
 }
