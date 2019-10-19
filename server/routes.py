f38336dda563b7468ed76fdbd35392dd177d3715
Ti-Chung Cheng <tcheng10@illinois.edu>
2019-10-19 09:59:13 -0500

    minor edit for clear submit data schema

--- server/routes.py
+++ server/routes.py
@@ -47,17 +47,28 @@ def welcome():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+	"""generic submit json to db
+	the json needs to specify where this json needs to go
+	"""
+
 	print(request.json)
 	insert_data = {
-		"form": "qv598"
+		"user_id": "",
+		"file_name":"",
+		"qid":"",
+		"results":{}
 	}
 	db.test.insert_one(insert_data)
 	return jsonify({'ok': True}), 200
 
+
 @app.route('/qv/<string:file_name>')
 def show_subpath(file_name):
-    file_name = '/'.join(['data', file_name])
-    filename = os.path.join(app.static_folder, file_name+'.json')
-    # data is a json file
-    data = json.load(open(filename))
-    return jsonify(data)
+	""" returns the json file appropriate to the question set it wants to generate
+	"""
+
+	file_name = '/'.join(['data', file_name])
+	filename = os.path.join(app.static_folder, file_name+'.json')
+	# data is a json file
+	data = json.load(open(filename))
+	return jsonify(data)
