d0ac05457554c36bb329ae7aee77b33a904b0631
Ti-Chung Cheng <tcheng10@illinois.edu>
2019-10-19 09:49:19 -0500

    create CreateUser Workflow

--- server/routes.py
+++ server/routes.py
@@ -1,20 +1,48 @@
-from flask import Flask, request, abort, jsonify
-from flask_cors import CORS
-import os
-from sys import exit
-import requests
 import json
-from server import app, db
 import json.decoder
+import os
+import random
 import uuid
 from pprint import pprint
-import random
+from sys import exit
 
+import requests
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS
+from datetime import datetime
+from server import app, db
+from server.utils import decide_path
 
 @app.route('/')
 def root():
     return jsonify({"string":"welcome"})
 	#return app.send_static_file('index.html')
+
+@app.route('/createUser', methods=['POST'])
+def welcome():
+	""" Once the user decides to move on, create a user and nsave to database.
+	return database object id as user id, flow.
+	"""
+
+	user = {
+		"userid": "",
+		"create_time": datetime.utcnow(),
+		"complete_flag": False
+	}
+
+	userid = db.user.insert_one(user).inserted_id
+
+	# update user path
+	user["path"] = decide_path()
+	user["userid"] = userid
+
+	db.user.update_one({
+	  '_id': userid
+	},{
+	'$set': user
+	}, upsert=False)
+
+	return jsonify(user), 200
 
 
 @app.route('/submit', methods=['POST'])
@@ -33,4 +61,3 @@ def show_subpath(file_name):
     # data is a json file
     data = json.load(open(filename))
     return jsonify(data)
-
